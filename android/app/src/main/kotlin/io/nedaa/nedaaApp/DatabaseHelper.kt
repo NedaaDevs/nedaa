f1fc1ec80cdacec58fe90679b5e3cca57cc7819e
Fahad <42780409+F2had@users.noreply.github.com>
2024-04-06 23:26:15 +0300

    feat: Add database helper to query prayer times

---
+++ android/app/src/main/kotlin/io/nedaa/nedaaApp/DatabaseHelper.kt
@@ -0,0 +1,83 @@
+package io.nedaa.nedaaApp
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import org.json.JSONObject
+
+// TODO: Better error handling(better?)
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "nedaa.db"
+        private const val TABLE_NAME = "PrayerTimes"
+    }
+
+    private lateinit var db: SQLiteDatabase
+
+
+    override fun onCreate(db: SQLiteDatabase) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    fun openDatabase() {
+        db = this.readableDatabase
+    }
+
+
+    fun getTimezone(): String? {
+        val cursor = db.query(
+            TABLE_NAME, // The table to query
+            null, // The columns to return
+            null, // The columns for the WHERE clause
+            null, // The values for the WHERE clause
+            null, // Don't group the rows
+            null, // Don't filter by row groups
+            null, // The sort order
+            "1" // Limit to the first result
+        )
+
+        var timezone: String? = null
+        if (cursor.moveToFirst()) {
+            val json = cursor.getString(cursor.getColumnIndex("content"))
+            val jsonObject = JSONObject(json)
+            timezone = jsonObject.getString("timezone")
+        }
+        cursor.close()
+        return timezone
+    }
+
+    fun getPrayerTimesForDate(date: Int): String? {
+        val cursor = db.query(
+            TABLE_NAME, // The table to query
+            null, // The columns to return
+            "date = ?", // The columns for the WHERE clause
+            arrayOf(date.toString()), // The values for the WHERE clause
+            null, // Don't group the rows
+            null, // Don't filter by row groups
+            null, // The sort order
+            "1" // Limit to the first result
+        )
+
+        var json: String? = null
+        if (cursor.moveToFirst()) {
+            json = cursor.getString(cursor.getColumnIndex("content"))
+
+        }
+        cursor.close()
+        return json
+    }
+
+    fun closeDatabase() {
+        if (this::db.isInitialized && db.isOpen) {
+            db.close()
+            println("Database closed")
+        }
+    }
+}
