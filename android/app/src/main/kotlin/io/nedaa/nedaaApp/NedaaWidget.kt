acfbc1b91f737f215afee970e31af7fff4a2738a
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-04-08 20:05:27 +0800

    feat: (widget) Add style with light,dark themes, and previousPrayer
    Improved the design, and also fetched the previous prayer, this design
    tries to be similar to the one on iOS.
    
    I'm not sure how we can set the theme, I'm assuming it will follow the
    device, but would be good to be able to configure it.
    
    Also, the time shown in the widget is not correct, I think we need to
    play around timezones.
    
    Then, next we need to have a dynamic counter

--- android/app/src/main/kotlin/io/nedaa/nedaaApp/NedaaWidget.kt
+++ android/app/src/main/kotlin/io/nedaa/nedaaApp/NedaaWidget.kt
@@ -1,9 +1,11 @@
 package io.nedaa.nedaaApp
 
-import HomeWidgetGlanceState
-import HomeWidgetGlanceStateDefinition
 import android.content.Context
 import android.net.Uri
+import HomeWidgetGlanceStateDefinition
+import HomeWidgetGlanceState
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -13,18 +15,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.action.ActionParameters
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.action.ActionCallback
-import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
@@ -33,9 +38,35 @@ import es.antonborri.home_widget.HomeWidgetBackgroundIntent
 import es.antonborri.home_widget.actionStartActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.format.DateTimeFormatter
 
 class NedaaWidget : GlanceAppWidget() {
     private lateinit var prayerService: PrayerTimeService
+
+    object ColorScheme {
+        private val DarkColors = darkColorScheme(
+            primary = Color(0xFFCBB279),
+            secondary = Color(0xFFEEEEEE),
+            tertiary = Color(0xFF537188),
+            onTertiary = Color(0xFFCBB279),
+            background = Color(0xFF537188),
+            tertiaryContainer = Color(0xFFE1D4BB),
+
+            )
+
+        private val LightColors = lightColorScheme(
+            primary = Color(0xFFCBB279),
+            secondary = Color(0xFFEEEEEE),
+            tertiary = Color(0xFFCBB279),
+            onTertiary = Color(0xFFEEEEEE),
+            background = Color(0xFFE1D4BB),
+            tertiaryContainer = Color(0xFF537188),
+        )
+
+        val colors = androidx.glance.material3.ColorProviders(
+            light = LightColors, dark = DarkColors
+        )
+    }
 
     /**
      * Needed for Updating
@@ -44,7 +75,9 @@ class NedaaWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            GlanceContent(context, currentState())
+            GlanceTheme(colors = ColorScheme.colors) {
+                GlanceContent(context, currentState())
+            }
         }
     }
 
@@ -53,58 +86,108 @@ class NedaaWidget : GlanceAppWidget() {
         val data = currentState.preferences
         prayerService = PrayerTimeService(context)
 
-        val nexPrayer = remember { mutableStateOf<Prayer?>(null) }
+        val nextPrayer = remember { mutableStateOf<Prayer?>(null) }
+        val prevPrayer = remember { mutableStateOf<Prayer?>(null) }
 
 //        Run the db query in the background thread
         LaunchedEffect(key1 = Unit) {
             prayerService.openDb()
-            nexPrayer.value = withContext(Dispatchers.IO) {
-                prayerService.getNextPrayer()
-
+            val both = withContext(Dispatchers.IO) {
                 val next = prayerService.getNextPrayer()
+                val prev = prayerService.getPreviousPrayer()
                 prayerService.closeDb()
-
-                return@withContext next
+                return@withContext Pair(next, prev)
             }
+            nextPrayer.value = both.first
+            prevPrayer.value = both.second
         }
 
-    
-        val nextPrayerName = nexPrayer.value?.name
-        val nextPrayerTime = nexPrayer.value?.dateTime
+        val nextPrayerName = nextPrayer.value?.name
+        val nextPrayerTime = nextPrayer.value?.dateTime
 
-        Box(
-            modifier = GlanceModifier.background(Color.White).padding(16.dp)
-                .clickable(onClick = actionStartActivity<MainActivity>(context))
+        val prevPrayerName = prevPrayer.value?.name
+        val prevPrayerTime = prevPrayer.value?.dateTime
+
+        println(nextPrayerName)
+        println(nextPrayerTime)
+        println(prevPrayerName)
+        println(prevPrayerTime)
+
+        val formatter = DateTimeFormatter.ofPattern("hh:mm a")
+
+
+        Content(
+            context,
+            prevPrayerName ?: "",
+            prevPrayerTime?.format(formatter) ?: "",
+            nextPrayerName ?: "",
+            nextPrayerTime?.format(formatter) ?: ""
+        )
+    }
+
+    @Composable
+    fun Content(
+        context: Context,
+        prevPrayerName: String,
+        prevPrayerTime: String,
+        nextPrayerName: String,
+        nextPrayerTime: String
+    ) {
+        Column(
+            modifier = GlanceModifier.background(GlanceTheme.colors.background).fillMaxSize()
+                .clickable(onClick = actionStartActivity<MainActivity>(context)),
+            horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
+            verticalAlignment = Alignment.Vertical.CenterVertically
         ) {
-            Column(
-                modifier = GlanceModifier.fillMaxSize(),
-                verticalAlignment = Alignment.Vertical.Top,
-                horizontalAlignment = Alignment.Horizontal.Start,
+            Box(
+                modifier = GlanceModifier.background(GlanceTheme.colors.tertiaryContainer)
+                    .cornerRadius(16.dp)
             ) {
-                Text(
-                    nextPrayerName ?: "",
-                    style = TextStyle(fontSize = 36.sp, fontWeight = FontWeight.Bold),
-//                        modifier = GlanceModifier.clickable(onClick = actionRunCallback<InteractiveAction>()),
-                )
-                Text(
-                    nextPrayerTime.toString() ?: "",
-                    style = TextStyle(fontSize = 36.sp, fontWeight = FontWeight.Bold),
-                )
 
+                Column(
+                    modifier = GlanceModifier.padding(32.dp, 4.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+                    ) {
+                    Text(
+                        text = prevPrayerName,
+                        modifier = GlanceModifier.padding(bottom = 4.dp),
+                        style = TextStyle(color = GlanceTheme.colors.tertiary)
+                    )
+                    Text(
+                        text = prevPrayerTime,
+                        modifier = GlanceModifier.padding(bottom = 4.dp),
+                        style = TextStyle(color = GlanceTheme.colors.onTertiary)
+                    )
+                }
             }
+
+            Spacer(modifier = GlanceModifier.height(16.dp))
+            Spacer(modifier = GlanceModifier.height(2.dp).background(GlanceTheme.colors.primary))
+            Spacer(modifier = GlanceModifier.height(12.dp))
+
+            Text(
+                text = nextPrayerName, style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = GlanceTheme.colors.tertiaryContainer
+                ), modifier = GlanceModifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = nextPrayerTime,
+                style = TextStyle(fontSize = 18.sp, color = GlanceTheme.colors.secondary),
+                modifier = GlanceModifier.padding(bottom = 4.dp)
+            )
         }
     }
 }
 
 class InteractiveAction : ActionCallback {
     override suspend fun onAction(
-        context: Context,
-        glanceId: GlanceId,
-        parameters: ActionParameters
+        context: Context, glanceId: GlanceId, parameters: ActionParameters
     ) {
         val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
-            context,
-            Uri.parse("nedaaWidget://titleClicked")
+            context, Uri.parse("nedaaWidget://titleClicked")
         )
         backgroundIntent.send()
     }
