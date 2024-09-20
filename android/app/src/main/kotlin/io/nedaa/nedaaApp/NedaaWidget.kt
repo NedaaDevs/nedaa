2e180f8e1bbe602b0d53b8dfde4aaf4b0c53cf56
Fahad <42780409+F2had@users.noreply.github.com>
2024-09-20 20:55:18 +0300

    fix: Previous prayer name and time color on dark mode

--- android/app/src/main/kotlin/io/nedaa/nedaaApp/NedaaWidget.kt
+++ android/app/src/main/kotlin/io/nedaa/nedaaApp/NedaaWidget.kt
@@ -94,8 +94,7 @@ class NedaaWidget : GlanceAppWidget() {
 
     @Composable
     private fun GlanceContent(
-        context: Context,
-        prayerService: PrayerTimeService
+        context: Context, prayerService: PrayerTimeService
     ) {
         val nextPrayer = remember { mutableStateOf<Prayer?>(null) }
         val prevPrayer = remember { mutableStateOf<Prayer?>(null) }
@@ -172,77 +171,66 @@ class NedaaWidget : GlanceAppWidget() {
         nextPrayerTime: String,
         isDark: Boolean
     ) {
-        val prevColor = if (isDark) GlanceTheme.colors.background else GlanceTheme.colors.primary
+        val backgroundColor = GlanceTheme.colors.background
+        val topBoxColor = GlanceTheme.colors.tertiary
+        val primaryTextColor = GlanceTheme.colors.primary
+        val secondaryTextColor = GlanceTheme.colors.secondary
+        val prevPrayerTextColor = if (isDark) backgroundColor else GlanceTheme.colors.primary
         Column(
-            modifier = GlanceModifier.background(GlanceTheme.colors.background).fillMaxSize(),
+            modifier = GlanceModifier.background(backgroundColor).fillMaxSize(),
             horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
             verticalAlignment = Alignment.Vertical.CenterVertically
         ) {
             Box(
-                modifier = GlanceModifier.background(GlanceTheme.colors.tertiary)
-                    .cornerRadius(16.dp)
+                modifier = GlanceModifier.background(topBoxColor).cornerRadius(16.dp)
             ) {
-
                 Column(
-                    modifier = GlanceModifier.padding(32.dp, 4.dp),
+                    modifier = GlanceModifier.padding(32.dp, 16.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
-
-                    ) {
+                ) {
                     Text(
-                        text = prevPrayerName,
-                        modifier = GlanceModifier.padding(bottom = 4.dp),
-                        style = TextStyle(
-                            color = prevColor,
+                        text = prevPrayerName, style = TextStyle(
+                            color = prevPrayerTextColor,
+                            fontSize = 16.sp,
                             fontWeight = FontWeight.Bold
                         )
                     )
                     Text(
-                        text = prevPrayerTime,
-                        modifier = GlanceModifier.padding(bottom = 4.dp),
-                        style = TextStyle(
-                            color = prevColor,
-                            fontWeight = FontWeight.Bold
+                        text = prevPrayerTime, style = TextStyle(
+                            color = prevPrayerTextColor,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Normal
                         )
                     )
                 }
             }
 
-            Spacer(modifier = GlanceModifier.height(16.dp))
+            Spacer(modifier = GlanceModifier.height(8.dp))
             Spacer(modifier = GlanceModifier.height(2.dp).background(GlanceTheme.colors.primary))
-            Spacer(modifier = GlanceModifier.height(12.dp))
+
+            Spacer(modifier = GlanceModifier.height(8.dp))
 
             Text(
                 text = nextPrayerName, style = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = GlanceTheme.colors.primary
-                ), modifier = GlanceModifier.padding(bottom = 4.dp)
+                    fontSize = 24.sp, fontWeight = FontWeight.Bold, color = primaryTextColor
+                )
             )
             Text(
-                text = nextPrayerTime,
-                style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = GlanceTheme.colors.secondary
-                ),
-                modifier = GlanceModifier.padding(bottom = 4.dp)
+                text = nextPrayerTime, style = TextStyle(
+                    fontSize = 28.sp, fontWeight = FontWeight.Normal, color = secondaryTextColor
+                )
             )
         }
     }
 
     private fun scheduleWidgetUpdate(context: Context, durationTillNextPrayer: Duration) {
-        val widgetUpdateWork = OneTimeWorkRequest.Builder(NedaaWidgetWorker::class.java)
-            .setInitialDelay(
-                durationTillNextPrayer.toMillis(),
-                java.util.concurrent.TimeUnit.MILLISECONDS
-            )
-            .addTag("WidgetUpdateWork")
-            .build()
+        val widgetUpdateWork =
+            OneTimeWorkRequest.Builder(NedaaWidgetWorker::class.java).setInitialDelay(
+                durationTillNextPrayer.toMillis(), java.util.concurrent.TimeUnit.MILLISECONDS
+            ).addTag("WidgetUpdateWork").build()
 
         WorkManager.getInstance(context).enqueueUniqueWork(
-            "WidgetUpdateWork",
-            ExistingWorkPolicy.REPLACE,
-            widgetUpdateWork
+            "WidgetUpdateWork", ExistingWorkPolicy.REPLACE, widgetUpdateWork
         )
 
 
@@ -250,8 +238,7 @@ class NedaaWidget : GlanceAppWidget() {
 }
 
 class NedaaWidgetWorker(
-    context: Context,
-    workerParams: WorkerParameters
+    context: Context, workerParams: WorkerParameters
 ) : CoroutineWorker(context, workerParams) {
 
     override suspend fun doWork(): Result {
