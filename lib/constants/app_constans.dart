f34579aea12654c6efb8adc655127bfc1bf604f1
Fahad <42780409+F2had@users.noreply.github.com>
2024-09-20 19:20:12 +0300

    refactor: Update android widgets names

--- lib/constants/app_constans.dart
+++ lib/constants/app_constans.dart
@@ -14,6 +14,6 @@ const List<String> iOSWidgetNames = [
 ];
 
 const List<String> androidWidgetNames = [
-  'NedaaWidget',
-  'AllPrayersWidget'
+  'NedaaWidgetReceiver',
+  'AllPrayersWidgetReceiver'
 ];
