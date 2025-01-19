ec7ed7810da11cff6d2f5c59dd8a4a7333fe49c4
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-19 23:56:27 +0300

    ci: Setup eslint, prettier, lint-staged and husky

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  ignorePatterns: ["/dist/*"],
+  env: {
+    jest: true,
+  },
+};
