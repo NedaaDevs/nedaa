9146ffbdafa80e0494f26a581e348dc4fe0ce1b6
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-20 05:22:23 +0300

    feat: Add gluestack ui provider with nativewind theme
    - Pass the selected theme mode to the UI provider
    
    - Add a new type for the AppMode enum "system"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
-import { Stack } from "expo-router";
-
+import "@/global.css";
 import "@/localization/i18n";
 
+import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+import { useAppStore } from "@/stores/app";
+
 export default function RootLayout() {
-  return <Stack />;
+  const { mode } = useAppStore();
+  return (
+    <GluestackUIProvider mode={mode}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </GluestackUIProvider>
+  );
 }
