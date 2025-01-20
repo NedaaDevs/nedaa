1abc9603658518a7cfd3ec68b0ff84b16e46cec9
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-20 15:45:42 +0300

    feat: Set direction based on locale
    - Remove setDirection as it begin handled by I18nManager

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,27 @@
+import { Stack } from "expo-router";
+import * as Updates from "expo-updates";
+import { I18nManager, Platform } from "react-native";
+
+import { getDirection, isRTL, useAppStore } from "@/stores/app";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 import "@/global.css";
 import "@/localization/i18n";
 
-import { Stack } from "expo-router";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-
-import { useAppStore } from "@/stores/app";
-
 export default function RootLayout() {
-  const { mode } = useAppStore();
+  const { mode, locale } = useAppStore();
+
+  const shouldBeRTL = isRTL(getDirection(locale));
+  if (shouldBeRTL !== I18nManager.isRTL && Platform.OS !== "web") {
+    I18nManager.allowRTL(shouldBeRTL);
+    I18nManager.forceRTL(shouldBeRTL);
+    I18nManager.swapLeftAndRightInRTL(shouldBeRTL);
+
+    (async function () {
+      await Updates.reloadAsync();
+    })();
+  }
+
   return (
     <GluestackUIProvider mode={mode}>
       <Stack
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,23 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTranslation } from "react-i18next";
+import { I18nManager, View, Text as T, StyleSheet } from "react-native";
+
 // Stores
 import { useAppStore } from "@/stores/app";
 
 // Enums
 import { AppLocale, AppMode } from "@/enums/app";
 
-import { useTranslation } from "react-i18next";
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
-import React from "react";
 import { Divider } from "@/components/ui/divider";
 import { Center } from "@/components/ui/center";
-import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function TabOneScreen() {
+export default function MainScreen() {
   const { locale, mode, setLocale, setMode, setIsFirstRun } = useAppStore();
 
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const toggleMode = () => {
     const modes = Object.values(AppMode);
@@ -29,34 +30,69 @@ export default function TabOneScreen() {
   const toggleLanguage = () => {
     // Simulate how is should be done
     setIsFirstRun(false);
-    const languages = Object.values(AppLocale);
+    const languages = Object.values(AppLocale).splice(0, 2);
 
     const currentIndex = languages.indexOf(locale);
     const nextIndex = (currentIndex + 1) % languages.length;
     setLocale(languages[nextIndex]);
-    i18n.changeLanguage(languages[nextIndex]);
   };
   return (
-    <SafeAreaView>
-      <Text className="bg-primary">
-        {t("deviceLocale")}: {t(`localeOptions.${locale}`)}
-      </Text>
-      <Box>
-        <Button onPress={toggleLanguage}>
-          <ButtonText>{t("toggleLanguage")}</ButtonText>
+    <SafeAreaView className="flex-1 bg-white px-4">
+      <View className="mt-5 mb-4">
+        <Text className="text-lg font-semibold text-gray-800 mb-3 text-left">
+          {t("deviceLocale")}: {t(`localeOptions.${locale}`)}
+        </Text>
+
+        <Button
+          className="rounded-lg py-4 bg-primary shadow-md active:opacity-80 h-14 min-h-[56px]"
+          onPress={toggleLanguage}
+        >
+          <ButtonText className="text-base font-medium text-center w-full px-4">
+            {t("toggleLanguage")}
+          </ButtonText>
         </Button>
-      </Box>
-      <Divider />
-      <Text className="bg-background-new">
-        {t("mode")}: {mode}
-      </Text>
-      <Center>
-        <Box className="bg-primary flex-1">
-          <Button action="primary" onPress={toggleMode}>
-            <ButtonText>{t("toggleMode")}</ButtonText>
-          </Button>
-        </Box>
-      </Center>
+      </View>
+
+      <Divider className="h-px bg-gray-200 my-5" />
+
+      <View className="mt-2">
+        <Text className="text-base text-gray-600 mb-4 text-start">
+          {t("mode")}: {mode}
+        </Text>
+
+        <Center>
+          <Box className="w-full px-1 mt-2">
+            <Button
+              action="primary"
+              className="rounded-lg py-2 bg-blue-500 shadow-md active:opacity-80"
+              onPress={toggleMode}
+            >
+              <ButtonText className="text-base font-medium text-start text-white">
+                {t("toggleMode")}
+              </ButtonText>
+            </Button>
+          </Box>
+        </Center>
+      </View>
+
+      <View style={styles.container}>
+        <T style={styles.paragraph}> {I18nManager.isRTL ? " RTL" : " LTR"}</T>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 8,
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "left",
+    width: "50%",
+    backgroundColor: "pink",
+  },
+});
