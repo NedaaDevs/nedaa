5a36a45efcd680560e310f32a9ad66b169ec2568
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-20 05:18:55 +0300

    feat: Add tailwind, nativewind and gluestack configuration files

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+/* eslint-env node */
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "./global.css" });
