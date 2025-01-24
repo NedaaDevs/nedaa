4b01c8d69564127075e9dd8852e925cb879a5c72
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-24 15:17:21 +0300

    feat: Add a utility func to map expo location permission status to local enum

---
+++ utils/location.ts
@@ -0,0 +1,15 @@
+import { PermissionStatus } from "expo-location";
+import { LocalPermissionStatus } from "@/enums/location";
+
+export const mapToLocalStatus = (
+  status: PermissionStatus,
+): LocalPermissionStatus => {
+  switch (status) {
+    case PermissionStatus.GRANTED:
+      return LocalPermissionStatus.GRANTED;
+    case PermissionStatus.DENIED:
+      return LocalPermissionStatus.DENIED;
+    default:
+      return LocalPermissionStatus.UNDETERMINED;
+  }
+};
