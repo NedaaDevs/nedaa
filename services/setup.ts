965e240227304b234bc2505b652f0dd61b2c87db
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-24 15:27:41 +0300

    feat: Update teh useInitialSetup hook to handle location permissions

--- services/setup.ts
+++ services/setup.ts
@@ -1,22 +1,32 @@
-import { AppState } from "@/stores/app";
-import { NotificationStore } from "@/stores/notification";
+import { LocationStore } from "@/types/location";
+import { AppState } from "@/types/app";
+import { NotificationState } from "@/types/notifications";
 
 import * as Sentry from "@sentry/react-native";
 
 export const performFirstRunSetup = async (
   appStore: AppState,
-  notificationStore: NotificationStore,
+  notificationStore: NotificationState,
+  locationStore: LocationStore,
 ) => {
   try {
     // Check existing notification permissions(This will update the store state with the current permissions)
     await notificationStore.checkPermissions();
+    await locationStore.checkPermissions();
 
     // Only request if first run and permissions aren't determined
     if (appStore.isFirstRun) {
-      const granted = await notificationStore.requestPermissions();
+      const grantedNotificationsPermission =
+        await notificationStore.requestPermissions();
+      const grantedLocationPermission =
+        await locationStore.requestPermissions();
 
-      if (!granted) {
-        console.log("User declined initial permission request");
+      if (!grantedNotificationsPermission) {
+        console.log("User declined initial notifications permission request");
+      }
+
+      if (!grantedLocationPermission) {
+        console.log("User declined initial location permission request");
       }
 
       // Mark first run as complete
