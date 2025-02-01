387d52f99bbdd74f55340d868cabbf88899f2407
Fahad <42780409+F2had@users.noreply.github.com>
2025-02-01 18:52:06 +0300

    feat: Add a date util to parse timestamp to date int like 20250201

---
+++ utils/date.ts
@@ -0,0 +1,12 @@
+import { format } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
+
+/**
+ * Converts a Unix timestamp (in seconds) to YYYYMMDD format
+ * @param {number} timestamp
+ * @param {string} timezone - target timezone
+ */
+export const timestampToDateInt = (timestamp: number, timezone: string): number => {
+  const date = toZonedTime(timestamp * 1000, timezone);
+  return parseInt(format(date, 'yyyyMMdd'), 10);
+};
