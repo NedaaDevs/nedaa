ae4236fe8c548879ae586bef2ccbb6f24176a541
Fahad <42780409+F2had@users.noreply.github.com>
2025-02-01 17:54:59 +0300

    refactor: Add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-  },
-  ignorePatterns: ["/dist/*"],
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+
+  ignorePatterns: ['/dist/*'],
   env: {
     jest: true,
   },
