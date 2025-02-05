94446c1a9aca87443b2662b0685b5e8a10181c2f
Fahad <42780409+F2had@users.noreply.github.com>
2025-02-05 06:46:27 +0300

    feat: Run prebuild to regenerate android folder with icon changes

--- android/app/src/main/java/dev/nedaa/app/MainActivity.kt
+++ android/app/src/main/java/dev/nedaa/app/MainActivity.kt
@@ -15,8 +15,7 @@ class MainActivity : ReactActivity() {
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     // setTheme(R.style.AppTheme);
-    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY)
-    // sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
     SplashScreenManager.registerOnActivity(this)
     // @generated end expo-splashscreen
     super.onCreate(null)
