23fc5b314f77227b781bc8cfaed9711729958e8f
Fahad <42780409+F2had@users.noreply.github.com>
2025-02-07 13:36:23 +0300

    chore: Get prayer times on first run

--- services/setup.ts
+++ services/setup.ts
@@ -4,11 +4,13 @@ import * as Sentry from "@sentry/react-native";
 import { LocationStore } from "@/types/location";
 import { AppState } from "@/types/app";
 import { NotificationState } from "@/types/notifications";
+import { PrayerTimesStore } from "@/types/prayerTimes";
 
 export const performFirstRunSetup = async (
   appStore: AppState,
   notificationStore: NotificationState,
-  locationStore: LocationStore
+  locationStore: LocationStore,
+  prayerTimesStore: PrayerTimesStore
 ) => {
   try {
     // Check existing notification permissions(This will update the store state with the current permissions)
@@ -25,9 +27,16 @@ export const performFirstRunSetup = async (
         console.log("User declined initial notifications permission request");
       }
 
-      if (!grantedLocationPermission) {
-        console.log("User declined initial location permission request");
+      // if the user granted location permission get current location
+      if (grantedLocationPermission) {
+        await locationStore.getCurrentLocation();
       }
+
+      // TODO: Remove just for testing now
+      await prayerTimesStore.getAndStorePrayerTimes({
+        lat: locationStore.locationDetails.coords.latitude,
+        long: locationStore.locationDetails.coords.longitude,
+      });
 
       // Mark first run as complete
       appStore.setIsFirstRun(false);
