85737344fdbf4c7312948103da5f495d25ee13dc
Fahad <42780409+F2had@users.noreply.github.com>
2025-03-05 09:19:05 +0300

    fix: Index page not rendering

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout() {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" />
       </Stack>
     </GluestackUIProvider>
   );
