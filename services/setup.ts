956ff9de38b73d8aa091d1965a23b0ff30e9bdd0
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-02 15:29:34 +0300

    refactor: Uncomment location and prayer times store imports and appSetup call

--- services/setup.ts
+++ services/setup.ts
@@ -1,18 +1,18 @@
 import * as Sentry from "@sentry/react-native";
 
-import { parseISO } from "date-fns";
-import { timeZonedNow } from "@/utils/date";
+// import { parseISO } from "date-fns";
+// import { timeZonedNow } from "@/utils/date";
 
 // Types
 import { LocationStore } from "@/stores/location";
 import { AppState } from "@/types/app";
-import { PrayerTimesStore } from "@/types/prayerTimes";
+import { PrayerTimesStore } from "@/stores/prayerTimes";
 
 // Utils
 import {
-  cancelAllScheduledNotifications,
+  // cancelAllScheduledNotifications,
   requestNotificationPermission,
-  scheduleNotification,
+  // scheduleNotification,
 } from "@/utils/notifications";
 import { requestLocationPermission } from "@/utils/location";
 
@@ -38,7 +38,7 @@ export const firstRunSetup = async (appStore: AppState) => {
 export const appSetup = async (locationStore: LocationStore, prayerStore: PrayerTimesStore) => {
   try {
     const { loadPrayerTimes, twoWeeksTimings } = prayerStore;
-    const { locationDetails } = locationStore;
+    // const { locationDetails } = locationStore;
 
     await loadPrayerTimes();
 
@@ -47,67 +47,67 @@ export const appSetup = async (locationStore: LocationStore, prayerStore: Prayer
       return;
     }
     // TODO: This will be refactored and moved to a better location(maybe we just call a func here that will handle scheduling logic)
-    const now = timeZonedNow(locationDetails.timezone);
-    const prayers = [];
-    const MAX_NOTIFICATIONS = 65;
+    // const now = timeZonedNow(locationDetails.timezone);
+    // const prayers = [];
+    // const MAX_NOTIFICATIONS = 65;
 
-    for (const prayerTiming of twoWeeksTimings) {
-      // Format date properly
-      const datePart = prayerTiming.date.toString().padStart(8, "0");
-      const year = datePart.substring(0, 4);
-      const month = datePart.substring(4, 6);
-      const day = datePart.substring(6, 8);
+    // for (const prayerTiming of twoWeeksTimings) {
+    //   // Format date properly
+    //   const datePart = prayerTiming.date.toString().padStart(8, "0");
+    //   const year = datePart.substring(0, 4);
+    //   const month = datePart.substring(4, 6);
+    //   const day = datePart.substring(6, 8);
 
-      // Process each prayer for this day
-      for (const [name, timeString] of Object.entries(prayerTiming.timings)) {
-        try {
-          // Parse the prayer time
-          let prayerTime;
-          if (timeString.includes("T")) {
-            // Already ISO format
-            prayerTime = parseISO(timeString);
-          } else {
-            // Need to combine date and time
-            prayerTime = parseISO(`${year}-${month}-${day}T${timeString}`);
-          }
+    //   // Process each prayer for this day
+    //   for (const [name, timeString] of Object.entries(prayerTiming.timings)) {
+    //     try {
+    //       // Parse the prayer time
+    //       let prayerTime;
+    //       if (timeString.includes("T")) {
+    //         // Already ISO format
+    //         prayerTime = parseISO(timeString);
+    //       } else {
+    //         // Need to combine date and time
+    //         prayerTime = parseISO(`${year}-${month}-${day}T${timeString}`);
+    //       }
 
-          // Add future prayers to our list
-          if (prayerTime > now) {
-            prayers.push({
-              name,
-              date: prayerTime,
-            });
+    //       // Add future prayers to our list
+    //       if (prayerTime > now) {
+    //         prayers.push({
+    //           name,
+    //           date: prayerTime,
+    //         });
 
-            // Stop once we reach the maximum number of notifications
-            if (prayers.length >= MAX_NOTIFICATIONS) {
-              break;
-            }
-          }
-        } catch {
-          // Silently ignore parsing errors
-        }
-      }
+    //         // Stop once we reach the maximum number of notifications
+    //         if (prayers.length >= MAX_NOTIFICATIONS) {
+    //           break;
+    //         }
+    //       }
+    //     } catch {
+    //       // Silently ignore parsing errors
+    //     }
+    //   }
 
-      // Also break out of the outer loop if we've reached max notifications
-      if (prayers.length >= MAX_NOTIFICATIONS) {
-        break;
-      }
-    }
+    //   // Also break out of the outer loop if we've reached max notifications
+    //   if (prayers.length >= MAX_NOTIFICATIONS) {
+    //     break;
+    //   }
+    // }
 
     // Only proceed if we have prayers to schedule
-    if (prayers.length > 0) {
-      await cancelAllScheduledNotifications();
+    // if (prayers.length > 0) {
+    //   await cancelAllScheduledNotifications();
 
-      await Promise.all(
-        prayers.map((prayer) =>
-          scheduleNotification(
-            prayer.date,
-            `Time for ${prayer.name}`,
-            `It's time to pray ${prayer.name}`
-          )
-        )
-      );
-    }
+    //   await Promise.all(
+    //     prayers.map((prayer) =>
+    //       scheduleNotification(
+    //         prayer.date,
+    //         `Time for ${prayer.name}`,
+    //         `It's time to pray ${prayer.name}`
+    //       )
+    //     )
+    //   );
+    // }
   } catch (error) {
     console.error("App setup failed:", error);
     Sentry.captureException(error);
