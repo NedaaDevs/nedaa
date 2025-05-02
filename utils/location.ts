79326b91da8fb382e5cae0f0497c9281f7715aa7
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-02 13:03:57 +0300

    feat: Add check/request location permission utils

--- utils/location.ts
+++ utils/location.ts
@@ -1,4 +1,10 @@
-import { PermissionStatus } from "expo-location";
+import {
+  PermissionStatus,
+  getForegroundPermissionsAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+
+// Enums
 import { LocalPermissionStatus } from "@/enums/location";
 
 export const mapToLocalStatus = (status: PermissionStatus): LocalPermissionStatus => {
@@ -9,5 +15,32 @@ export const mapToLocalStatus = (status: PermissionStatus): LocalPermissionStatu
       return LocalPermissionStatus.DENIED;
     default:
       return LocalPermissionStatus.UNDETERMINED;
+  }
+};
+
+export const checkLocationPermission = async () => {
+  try {
+    const { status, canAskAgain } = await getForegroundPermissionsAsync();
+
+    return {
+      granted: status === PermissionStatus.GRANTED,
+      canRequestAgain: canAskAgain,
+    };
+  } catch (error) {
+    console.error("Location Permission check failed: ", error);
+    return {
+      granted: false,
+      canRequestAgain: true,
+    };
+  }
+};
+
+export const requestLocationPermission = async () => {
+  try {
+    const { status } = await requestForegroundPermissionsAsync();
+    return { granted: status === PermissionStatus.GRANTED };
+  } catch (error) {
+    console.error("Location permission request failed:", error);
+    return { granted: false, error };
   }
 };
