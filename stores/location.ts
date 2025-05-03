91d88e805f813ba8992d21b242379426f61a1b3b
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-03 10:06:46 +0300

    feat: Get geocode info when updating location with current locale

--- stores/location.ts
+++ stores/location.ts
@@ -6,10 +6,19 @@ import * as Location from "expo-location";
 // Types
 import { LocationDetails, initialLocationDetails } from "@/types/location";
 
+// Stores
+import appStore from "@/stores/app";
+
+// Services
+import { geocodeApi } from "@/api/geocodeApi";
+import { ReverseGeocodeParams, ReverseGeocodeResponse } from "@/types/geocode";
+import { ErrorResponse } from "@/types/api";
+
 export type LocationStore = {
   locationDetails: LocationDetails;
   isGettingLocation: boolean;
   updateLocation: () => Promise<void>;
+  reverseGeocode: (params: ReverseGeocodeParams) => Promise<ReverseGeocodeResponse>;
   setTimezone: (timezone: string) => Promise<void>;
 };
 
@@ -22,11 +31,34 @@ export const useLocationStore = create<LocationStore>()(
 
         updateLocation: async () => {
           try {
-            // TODO: Use locale to get localized city name, and subscribe to locale state
-            // to update geocoding results
             const location = await Location.getCurrentPositionAsync({
               accuracy: Location.Accuracy.Low,
             });
+
+            try {
+              // Now we have the coordinates we can reverse geocode to get city name.
+              // preferably if we get a localized version to match the current app locale
+              const geocodeAdd = await get().reverseGeocode({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                locale: appStore.getState().locale,
+              });
+              set({
+                locationDetails: {
+                  coords: location.coords,
+                  address: {
+                    country: geocodeAdd.countryName,
+                    city: geocodeAdd.city,
+                  },
+                  error: null,
+                  isLoading: false,
+                  timezone: geocodeAdd.timezone,
+                },
+              });
+              return;
+            } catch {
+              // if api reverse geocode failed(general error, rate limit etc) silently ignore error and continue
+            }
 
             const [geocodedAddress] = await Location.reverseGeocodeAsync({
               latitude: location.coords.latitude,
@@ -37,7 +69,10 @@ export const useLocationStore = create<LocationStore>()(
             set({
               locationDetails: {
                 coords: location.coords,
-                address: geocodedAddress,
+                address: {
+                  country: geocodedAddress.country ?? "N/A",
+                  city: geocodedAddress.city ?? "N/A",
+                },
                 error: null,
                 isLoading: false,
                 timezone,
@@ -59,6 +94,25 @@ export const useLocationStore = create<LocationStore>()(
           }
         },
 
+        reverseGeocode: async (params) => {
+          try {
+            const response = await geocodeApi.reverseGeocode({
+              latitude: params.latitude,
+              longitude: params.longitude,
+              locale: params.locale,
+            });
+
+            if (!response.success) {
+              throw response as ErrorResponse;
+            }
+
+            return response.data;
+          } catch (error: any) {
+            console.error("Failed reverse geocode: ", error);
+            throw error as ErrorResponse;
+          }
+        },
+
         setTimezone: async (timezone: string) => {
           const locationDetails = get().locationDetails;
           set({
@@ -69,6 +123,7 @@ export const useLocationStore = create<LocationStore>()(
           });
         },
       }),
+
       {
         name: "location-storage",
         storage: createJSONStorage(() => Storage),
