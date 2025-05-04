bb2c8ca93113dc26f96254b03880fc2816b493d6
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 15:31:40 +0300

    feat: Replace placeholder with SettingsScreen layout and language settings

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -1,12 +1,27 @@
+import { ScrollView } from "react-native";
+
+// Plugins
+import { useTranslation } from "react-i18next";
+
 // Components
-import { Box } from "@/components/ui/box";
-import { Text } from "@/components/ui/text";
+import { Divider } from "@/components/ui/divider";
+
+import TopBar from "@/components/TopBar";
+
+// Icons
+import { Languages, ArrowRight } from "lucide-react-native";
+import SettingsItem from "@/components/SettingsItem";
 
 const SettingsScreen = () => {
+  const { t } = useTranslation();
   return (
-    <Box>
-      <Text>Settings Screen</Text>
-    </Box>
+    <ScrollView className="bg-grey dark:bg-black" contentContainerStyle={{ flexGrow: 1 }}>
+      <TopBar title={t("settings")} href="/" icon={ArrowRight} backOnClick />
+
+      {/* Language */}
+      <SettingsItem name={t("language")} path="/settings/language" icon={Languages} />
+      <Divider className="bg-primary dark:bg-secondary mx-2" />
+    </ScrollView>
   );
 };
 
