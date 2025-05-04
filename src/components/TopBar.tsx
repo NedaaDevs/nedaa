e4e339a9851c4e1bbe9d2e0f24199a3ee1a7b655
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 10:33:29 +0300

    feat: Navigate to settings page on icon press

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,18 +1,26 @@
+// Plugins
+import { useTranslation } from "react-i18next";
+import { Link } from "expo-router";
+
 // Components
 import { Box } from "@/components/ui/box";
 import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
-import { Settings } from "lucide-react-native";
+import { Pressable } from "@/components/ui/pressable";
 
-// Plugins
-import { useTranslation } from "react-i18next";
+// Icons
+import { Settings } from "lucide-react-native";
 
 const TopBar = () => {
   const { t } = useTranslation();
   return (
     <Box className="px-4 py-3 flex flex-row items-center justify-between bg-background">
       <Box className="flex-1 items-start">
-        <Icon as={Settings} size="md" color="white" />
+        <Link href="/settings" asChild>
+          <Pressable>
+            <Icon as={Settings} size="md" color="white" />
+          </Pressable>
+        </Link>
       </Box>
       <Box className="flex-1 items-center">
         <Text className="text-xl font-bold text-white ">{t("nedaa")}</Text>
