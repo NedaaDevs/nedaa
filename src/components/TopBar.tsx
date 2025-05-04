3f25d14607c14bb3268f32ec7f2689be5f891c75
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 11:56:43 +0300

    refactor: AppBar to be reusable

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -8,22 +8,26 @@ import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 import { Pressable } from "@/components/ui/pressable";
 
-// Icons
-import { Settings } from "lucide-react-native";
+type Props = {
+  // TODO: Add type for href
+  href: "/settings" | "/";
+  title: string;
+  icon: any;
+};
 
-const TopBar = () => {
+const TopBar = ({ href, title, icon }: Props) => {
   const { t } = useTranslation();
   return (
     <Box className="px-4 py-3 flex flex-row items-center justify-between bg-background">
       <Box className="flex-1 items-start">
-        <Link href="/settings" asChild>
+        <Link href={href} asChild>
           <Pressable>
-            <Icon as={Settings} size="md" color="white" />
+            <Icon as={icon} size="md" color="white" />
           </Pressable>
         </Link>
       </Box>
       <Box className="flex-1 items-center">
-        <Text className="text-xl font-bold text-white ">{t("nedaa")}</Text>
+        <Text className="text-xl font-bold text-white ">{title}</Text>
       </Box>
       <Box className="flex-1 items-end" />
     </Box>
