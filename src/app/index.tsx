e9900abecdce77aa99d4c2797aac81f660b3e3dc
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 11:57:06 +0300

    feat: Pass new props to top bar component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,28 @@
 import { ScrollView } from "react-native";
+
+// Components
 import { Divider } from "@/components/ui/divider";
 import { Box } from "@/components/ui/box";
 import TopBar from "@/components/TopBar";
 import Header from "@/components/Header";
 import TimingsCarousel from "@/components/TimingsCarousel";
 
+// Plugins
+import { useTranslation } from "react-i18next";
+
 // Stores
 import { useAppStore } from "@/stores/app";
 
+// Icons
+import { Settings } from "lucide-react-native";
 export default function MainScreen() {
   const { mode } = useAppStore();
 
+  const { t } = useTranslation();
+
   return (
     <ScrollView className="bg-grey dark:bg-black" contentContainerStyle={{ flexGrow: 1 }}>
-      <TopBar />
+      <TopBar title={t("nedaa")} href="/settings" icon={Settings} />
       <Box className="flex-1">
         <Header />
         <Divider />
