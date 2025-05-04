ca7c991ea2ce2dec364dd71b8c3de6456652ccb1
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 12:46:24 +0300

    feat: Add backOnClick props to top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 // Plugins
 import { useTranslation } from "react-i18next";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 // Components
 import { Box } from "@/components/ui/box";
@@ -13,21 +13,36 @@ type Props = {
   href: "/settings" | "/";
   title: string;
   icon: any;
+  backOnClick?: boolean;
 };
 
-const TopBar = ({ href, title, icon }: Props) => {
+const TopBar = ({ href, title, icon, backOnClick = false }: Props) => {
   const { t } = useTranslation();
+  const router = useRouter();
+
+  const handlePress = () => {
+    if (backOnClick) {
+      router.back();
+    }
+  };
+
   return (
     <Box className="px-4 py-3 flex flex-row items-center justify-between bg-background">
       <Box className="flex-1 items-start">
-        <Link href={href} asChild>
-          <Pressable>
+        <Pressable onPress={handlePress}>
+          {backOnClick ? (
             <Icon as={icon} size="md" color="white" />
-          </Pressable>
-        </Link>
+          ) : (
+            <Link href={href} asChild>
+              <Pressable>
+                <Icon as={icon} size="md" color="white" />
+              </Pressable>
+            </Link>
+          )}
+        </Pressable>
       </Box>
       <Box className="flex-1 items-center">
-        <Text className="text-xl font-bold text-white ">{title}</Text>
+        <Text className="text-xl font-bold text-white">{title}</Text>
       </Box>
       <Box className="flex-1 items-end" />
     </Box>
