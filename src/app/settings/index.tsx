b71d6094c3db64b3d929ddf5956e3c9e6b9ec385
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 17:13:55 +0300

    feat: Render theme settings in settings screen

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -9,18 +9,20 @@ import { Divider } from "@/components/ui/divider";
 import TopBar from "@/components/TopBar";
 
 // Icons
-import { Languages, ArrowRight } from "lucide-react-native";
+import { Languages, Palette } from "lucide-react-native";
 import SettingsItem from "@/components/SettingsItem";
 
 const SettingsScreen = () => {
   const { t } = useTranslation();
   return (
     <ScrollView className="bg-grey dark:bg-black" contentContainerStyle={{ flexGrow: 1 }}>
-      <TopBar title={t("settings")} href="/" icon={ArrowRight} backOnClick />
+      <TopBar title="settings" href="/" backOnClick />
 
       {/* Language */}
       <SettingsItem name={t("language")} path="/settings/language" icon={Languages} />
-      <Divider className="bg-primary dark:bg-secondary mx-2" />
+      <Divider className="bg-primary/30 dark:bg-secondary/30 mx-4 w-[calc(100%-32px)]" />
+      {/* Theme */}
+      <SettingsItem name={t("appearance")} path="/settings/theme" icon={Palette} />
     </ScrollView>
   );
 };
