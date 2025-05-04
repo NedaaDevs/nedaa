2c2e1dac2b2f36bc75324af94ffb7c12bc5b47b8
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 10:22:57 +0300

    feat: Add settings screen index page

---
+++ src/app/settings/index.tsx
@@ -0,0 +1,13 @@
+// Components
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+
+const SettingsScreen = () => {
+  return (
+    <Box>
+      <Text>Settings Screen</Text>
+    </Box>
+  );
+};
+
+export default SettingsScreen;
