ddc7560f4dcea61c136bdf5355aa54d8d8a81e0f
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 17:12:37 +0300

    feat: Add theme list component with translations

---
+++ src/components/ThemeList.tsx
@@ -0,0 +1,69 @@
+// Plugins
+import { useTranslation } from "react-i18next";
+
+// Components
+import { ActionsheetFlatList } from "@/components/ui/actionsheet";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import { Icon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+
+// Icons
+import { Check } from "lucide-react-native";
+
+// Enums
+import { AppMode } from "@/enums/app";
+
+// Stores
+import { useAppStore } from "@/stores/app";
+
+type ItemType = {
+  id: AppMode;
+  title: string;
+  description: string;
+};
+
+const ThemeList = () => {
+  const { mode, setMode } = useAppStore();
+  const { t } = useTranslation();
+
+  const modeData: ItemType[] = Object.values(AppMode).map((modeCode) => {
+    return {
+      id: modeCode as AppMode,
+      title: t(`themes.${modeCode}.title`),
+      description: t(`themes.${modeCode}.description`),
+    };
+  });
+
+  const handleSelectTheme = (item: ItemType) => {
+    setMode(item.id);
+  };
+
+  return (
+    <Box className="bg-white dark:bg-gray-800 mt-2 rounded-lg">
+      <ActionsheetFlatList
+        data={modeData}
+        renderItem={({ item, index }: any) => (
+          <Pressable
+            onPress={() => handleSelectTheme(item)}
+            className={`py-4 px-4 flex-row justify-between items-center ${
+              index < modeData.length - 1 ? "border-b border-gray-200 dark:border-gray-700" : ""
+            }`}>
+            <Box>
+              <Text className="text-lg font-semibold text-typography dark:text-tertiary">
+                {item.title}
+              </Text>
+              <Text className="text-base text-gray-500 dark:text-gray-400">{item.description}</Text>
+            </Box>
+            {mode === item.id && (
+              <Icon as={Check} className="color-primary dark:color-secondary" size="md" />
+            )}
+          </Pressable>
+        )}
+        keyExtractor={(item: any) => item.id}
+      />
+    </Box>
+  );
+};
+
+export default ThemeList;
