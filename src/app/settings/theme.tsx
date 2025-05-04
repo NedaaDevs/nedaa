9272e413feb79d40b5698555cd76db8d228cd065
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-04 17:13:33 +0300

    feat: Add Theme settings item

---
+++ src/app/settings/theme.tsx
@@ -0,0 +1,15 @@
+// Components
+import { Box } from "@/components/ui/box";
+import ThemeList from "@/components/ThemeList";
+import TopBar from "@/components/TopBar";
+
+const ThemeSettings = () => {
+  return (
+    <Box className="flex-1 bg-grey dark:bg-black">
+      <TopBar title="appearance" href="/" backOnClick />
+      <ThemeList />
+    </Box>
+  );
+};
+
+export default ThemeSettings;
