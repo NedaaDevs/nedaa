f57b81de86b3adf48309f9e8ff9fa0eab781cc36
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-05 09:27:29 +0300

    feat: Refactor location update logic to improve address translation handling

--- src/stores/location.ts
+++ src/stores/location.ts
@@ -18,6 +18,7 @@ export type LocationStore = {
   locationDetails: LocationDetails;
   isGettingLocation: boolean;
   updateLocation: () => Promise<void>;
+  updateAddressTranslation: () => Promise<boolean>;
   reverseGeocode: (params: ReverseGeocodeParams) => Promise<ReverseGeocodeResponse>;
   setTimezone: (timezone: string) => Promise<void>;
 };
@@ -35,30 +36,17 @@ export const useLocationStore = create<LocationStore>()(
               accuracy: Location.Accuracy.Low,
             });
 
-            try {
-              // Now we have the coordinates we can reverse geocode to get city name.
-              // preferably if we get a localized version to match the current app locale
-              const geocodeAdd = await get().reverseGeocode({
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                locale: appStore.getState().locale,
+            // Now we have the coordinates we can reverse geocode to get city name.
+            // preferably if we get a localized version to match the current app locale
+            const success = await get()
+              .updateAddressTranslation()
+              .catch((e) => {
+                console.error(e);
+                return false;
               });
-              set({
-                locationDetails: {
-                  coords: location.coords,
-                  address: {
-                    country: geocodeAdd.countryName,
-                    city: geocodeAdd.city,
-                  },
-                  error: null,
-                  isLoading: false,
-                  timezone: geocodeAdd.timezone,
-                },
-              });
-              return;
-            } catch {
-              // if api reverse geocode failed(general error, rate limit etc) silently ignore error and continue
-            }
+
+            // if the translation successful, we can skip the geocoding
+            if (success) return;
 
             const [geocodedAddress] = await Location.reverseGeocodeAsync({
               latitude: location.coords.latitude,
@@ -92,6 +80,36 @@ export const useLocationStore = create<LocationStore>()(
               isGettingLocation: false,
             });
           }
+        },
+        updateAddressTranslation: async () => {
+          const location = get().locationDetails;
+
+          const geocodeAdd = await get()
+            .reverseGeocode({
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              locale: appStore.getState().locale,
+            })
+
+            .catch((e) => console.error(e));
+
+          if (geocodeAdd) {
+            set({
+              locationDetails: {
+                coords: location.coords,
+                address: {
+                  country: geocodeAdd.countryName,
+                  city: geocodeAdd.city,
+                },
+                error: null,
+                isLoading: false,
+                timezone: geocodeAdd.timezone,
+              },
+            });
+            return true;
+          }
+
+          return false;
         },
 
         reverseGeocode: async (params) => {
