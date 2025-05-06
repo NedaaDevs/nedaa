2a7bc8d4cbe2d39532ece2e13b91926f71b89f7f
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-06 19:14:01 +0300

    feat: Add currentValue prop to SettingsItem for dynamic language and theme display

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -5,24 +5,37 @@ import { useTranslation } from "react-i18next";
 
 // Components
 import { Divider } from "@/components/ui/divider";
-
 import TopBar from "@/components/TopBar";
+import SettingsItem from "@/components/SettingsItem";
 
 // Icons
 import { Languages, Palette } from "lucide-react-native";
-import SettingsItem from "@/components/SettingsItem";
+
+// Stores
+import { useAppStore } from "@/stores/app";
 
 const SettingsScreen = () => {
   const { t } = useTranslation();
+  const { locale, mode } = useAppStore();
   return (
     <ScrollView className="bg-grey dark:bg-black" contentContainerStyle={{ flexGrow: 1 }}>
       <TopBar title="settings" href="/" backOnClick />
 
       {/* Language */}
-      <SettingsItem name={t("language")} path="/settings/language" icon={Languages} />
+      <SettingsItem
+        name={t("language")}
+        path="/settings/language"
+        icon={Languages}
+        currentValue={t(`languages.${locale}.nativeTitle`)}
+      />
       <Divider className="bg-primary/30 dark:bg-secondary/30 mx-4 w-[calc(100%-32px)]" />
       {/* Theme */}
-      <SettingsItem name={t("appearance")} path="/settings/theme" icon={Palette} />
+      <SettingsItem
+        name={t("appearance")}
+        path="/settings/theme"
+        icon={Palette}
+        currentValue={t(`themes.${mode}.title`)}
+      />
     </ScrollView>
   );
 };
