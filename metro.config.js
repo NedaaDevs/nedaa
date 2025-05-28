b67fa0b682ddd024dc784dd934893478c981a87c
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-28 20:54:43 +0300

    build: Configure metro-minify-terser for improved JavaScript minification

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,23 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./assets/global.css" });
+const configWithMinifier = {
+  ...config,
+  transformer: {
+    ...config.transformer,
+    minifierPath: "metro-minify-terser",
+    minifierConfig: {
+      compress: {
+        drop_console: true,
+        pure_funcs: ["console.log", "console.info", "console.debug"],
+        warnings: false,
+        passes: 2,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
+};
+
+module.exports = withNativeWind(configWithMinifier, { input: "./assets/global.css" });
