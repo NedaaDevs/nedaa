e2df6940153053cb0a65d0c6e61dbeb549537a99
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-31 00:39:39 +0300

    feat: Fetch and store providers

--- src/api/prayerTimes.api.ts
+++ src/api/prayerTimes.api.ts
@@ -4,9 +4,11 @@ import { apiGet } from "@/services/api";
 import { PRAYER_TIMES } from "@/constants/ApiRoutes";
 
 // Types
-import type { PrayerTimesResponse, PrayerTimesParams } from "@/types/prayerTimes";
+import type { PrayerTimesResponse, PrayerTimesParams, Provider } from "@/types/prayerTimes";
 
 export const prayerTimesApi = {
   get: (params: PrayerTimesParams) =>
     apiGet<PrayerTimesResponse>(PRAYER_TIMES.GET_PRAYER_TIMES, params),
+
+  getProviders: () => apiGet<Provider[]>(PRAYER_TIMES.PROVIDERS),
 };
