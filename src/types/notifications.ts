80750d384b03657e5576bbcb476f2222c32b6e9c
Fahad <42780409+F2had@users.noreply.github.com>
2025-06-08 17:36:10 +0300

    refactor: Update notification types and store structure

--- src/types/notifications.ts
+++ src/types/notifications.ts
@@ -1,6 +1,19 @@
 import { LocalPermissionStatus } from "@/enums/notifications";
 
+import type { NotificationRequest } from "expo-notifications";
+
 export type NotificationPermissionsState = {
   status: LocalPermissionStatus;
   canRequestAgain: boolean;
+};
+
+export type NotificationState = {
+  isScheduling: boolean;
+};
+
+export type NotificationAction = {
+  scheduleTestNotification: () => Promise<void>;
+  getScheduledNotifications: () => Promise<NotificationRequest[]>;
+  clearNotifications: () => Promise<void>;
+  openNotificationSettings: () => Promise<void>;
 };
