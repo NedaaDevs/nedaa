578a1910f13c0b7d070e13b22659c5535140c2b8
Fahad <42780409+F2had@users.noreply.github.com>
2025-06-10 08:32:08 +0300

    chore: Regenerate android folder

--- android/app/src/main/java/dev/nedaa/app/MainApplication.kt
+++ android/app/src/main/java/dev/nedaa/app/MainApplication.kt
@@ -2,40 +2,40 @@ package dev.nedaa.app
 
 import android.app.Application
 import android.content.res.Configuration
+
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-          ReactNativeHostWrapper(
-                  this,
-                  object : DefaultReactNativeHost(this) {
-                    override fun getPackages(): List<ReactPackage> {
-                      val packages = PackageList(this).packages
-                      // Packages that cannot be autolinked yet can be added manually here, for
-                      // example:
-                      // packages.add(new MyReactNativePackage());
-                      return packages
-                    }
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+        this,
+        object : DefaultReactNativeHost(this) {
+          override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(MyReactNativePackage())
+            return packages
+          }
 
-                    override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-                    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-                    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-                    override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-                  }
-          )
+          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+  )
 
   override val reactHost: ReactHost
     get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
