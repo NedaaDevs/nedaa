94e0d988a4c923088457efaf2056c301295ee382
Fahad <42780409+F2had@users.noreply.github.com>
2025-06-10 08:32:08 +0300

    chore: Regenerate android folder

--- android/app/src/main/java/dev/nedaa/app/MainActivity.kt
+++ android/app/src/main/java/dev/nedaa/app/MainActivity.kt
@@ -1,13 +1,15 @@
 package dev.nedaa.app
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
 import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 import expo.modules.ReactActivityDelegateWrapper
-import expo.modules.splashscreen.SplashScreenManager
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,29 +35,31 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
-            this,
-            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-            object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {}
-    )
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
   }
 
   /**
-   * Align the back button behavior with Android S where moving root activities to background
-   * instead of finishing activities.
-   * @see <a
-   * href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
-   */
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
   override fun invokeDefaultOnBackPressed() {
-    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-      if (!moveTaskToBack(false)) {
-        // For non-root activities, use the default implementation to finish them.
-        super.invokeDefaultOnBackPressed()
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
       }
-      return
-    }
 
-    // Use the default back button implementation on Android S
-    // because it's doing more than [Activity.moveTaskToBack] in fact.
-    super.invokeDefaultOnBackPressed()
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
   }
 }
