7ebfb62e07b029e9f8cd0d45864a10bbeef70d91
Fahad <42780409+F2had@users.noreply.github.com>
2025-06-25 23:50:35 +0300

    feat(AdvanceSettings): Add Hijri settings item to advance settings page

--- src/app/settings/advance/index.tsx
+++ src/app/settings/advance/index.tsx
@@ -3,9 +3,9 @@ import { useTranslation } from "react-i18next";
 
 // Components
 import { Background } from "@/components/ui/background";
+import { Divider } from "@/components/ui/divider";
 
 import TopBar from "@/components/TopBar";
-import { ProviderSettings } from "@/components/ProviderSettings";
 import SettingsItem from "@/components/SettingsItem";
 
 const AdvanceSettings = () => {
@@ -14,6 +14,8 @@ const AdvanceSettings = () => {
     <Background>
       <TopBar title="settings.advance.title" href="/settings" backOnClick />
       <SettingsItem name={t("settings.advance.provider.title")} path="/settings/advance/provider" />
+      <Divider />
+      <SettingsItem name={t("settings.advance.hijri.title")} path="/settings/advance/hijri" />
     </Background>
   );
 };
