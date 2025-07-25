3de62341a4dabef5e5d6d71200ca53d321c3bc85
Fahad <42780409+F2had@users.noreply.github.com>
2025-07-25 22:26:49 +0300

    feat: Add new db tables constants

--- src/constants/DB.ts
+++ src/constants/DB.ts
@@ -1,2 +1,5 @@
 export const DB_NAME = "nedaa.db" as const;
 export const TABLE_NAME = "prayer_times" as const;
+
+export const ATHKAR_STREAK_TABLE = "athkar_streak";
+export const ATHKAR_COMPLETION_HISTORY_TABLE = "athkar_completion_history";
