67ed7cc9d485dee551ce9e137cd152190e1c465d
Fahad <42780409+F2had@users.noreply.github.com>
2025-10-29 22:05:48 +0300

    feat: Initialize QadaDB in the useInitialSetup hook

--- src/hooks/useInitialSetup.ts
+++ src/hooks/useInitialSetup.ts
@@ -9,6 +9,7 @@ import { useNotificationStore } from "@/stores/notification";
 // Services
 import { appSetup, firstRunSetup } from "@/services/setup";
 import { PrayerTimesDB } from "@/services/db";
+import { QadaDB } from "@/services/qada-db";
 
 const initSentry = (consent: boolean) => {
   if (!__DEV__ && consent) {
@@ -21,6 +22,7 @@ const initSentry = (consent: boolean) => {
 
 const initDB = async () => {
   await PrayerTimesDB.initialize();
+  await QadaDB.initialize();
 };
 
 export const useInitialSetup = () => {
