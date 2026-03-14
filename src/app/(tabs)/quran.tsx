18cbe591d16dc482ed2d6e182c8777d38f3477d9
Fahad <42780409+F2had@users.noreply.github.com>
2026-03-14 20:00:13 +0300

    feat(quran): add Quran tab, move Qada to Tools

---
+++ src/app/(tabs)/quran.tsx
@@ -0,0 +1,12 @@
+import { YStack } from "tamagui";
+import { Text } from "@/components/ui/text";
+
+const QuranScreen = () => {
+  return (
+    <YStack flex={1} justifyContent="center" alignItems="center">
+      <Text>Quran Reader</Text>
+    </YStack>
+  );
+};
+
+export default QuranScreen;
